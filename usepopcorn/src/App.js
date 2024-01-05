900926d4bfd5f39e1c3129a16c30e3c66a38737c
Ravi Kumar <ravi7284007@gmail.com>
2024-01-05 06:51:52 +0530

    Fetched data from API

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { tempMovieData, tempWatchedData } from "./components/data";
 import NumResult from "./components/navbar/NumResult";
@@ -10,25 +10,87 @@ import Box from "./components/Box";
 import WatchedSummary from "./components/WatchList/WatchedSummary";
 import WatchedMovieList from "./components/WatchList/WatchedMovieList";
 
+const KEY = `a15d2a7`;
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [query, setQuery] = useState("inception");
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [selectedId, setSelectedID] = useState(null);
+
+  useEffect(
+    function () {
+      const fetchMovie = async () => {
+        try {
+          setIsLoading(true);
+          setError("");
+
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with Fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+        } catch (error) {
+          console.error(error.message);
+          setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      fetchMovie();
+    },
+    [query]
+  );
 
   return (
     <>
       <Navbar>
         <Logo />
-        <Search />
+        <Search setQuery={setQuery} query={query} />
         <NumResult movies={movies} />
       </Navbar>
 
       <Main>
-        <Box element={<MovieList movies={movies} />} />
+        {/* {isLoading ? (
+          <Loader />
+        ) : (
+          <Box element={<MovieList movies={movies} />} />
+        )} */}
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
+        </Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
         </Box>
       </Main>
     </>
+  );
+}
+
+function Loader() {
+  return <p className="loader">Loading.....</p>;
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>⛔</span> {message}
+    </p>
   );
 }
--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import RatingStars from "./RatingStars";
+// import RatingStars from "./RatingStars";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <RatingStars maxRating={9} />
-    <RatingStars maxRating={10} />
-  </React.StrictMode>
+  <>
+    <App />
+    {/* <RatingStars maxRating={9} /> */}
+    {/* <RatingStars maxRating={10} /> */}
+  </>
 );
